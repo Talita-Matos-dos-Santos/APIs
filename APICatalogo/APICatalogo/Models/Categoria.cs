ceb7605c068b710af92e0eddbdaa38a3a0be6fba
Talita Matos <talitagvmatos@gmail.com>
2023-06-26 19:13:16 -0300

    Usando DataAnnotations. Migration AjusteTabelas feito.

--- APICatalogo/APICatalogo/Models/Categoria.cs
+++ APICatalogo/APICatalogo/Models/Categoria.cs
@@ -1,11 +1,19 @@
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace APICatalogo.Models;
 
 public class Categoria
 {
     public int CategoriaId { get; set; } //EF por convencao vai adotar como chave primária
+    
+    [Required] //o campo passa a ser obrigatorio, indica com NotNull
+    [StringLength(80)] //define q o tamanho padrao maximo é de 80
     public string? Nome { get; set; } //sem a interrogacao o compilador da o alerta que ta non nullable, isso pq la em APICatalogo.csproj tá definido que as propriedades por referencia deverao ser nullable. colocando a ? eu defino a propriedade como nullable.
+    
+    [Required] //o campo é obrigatório
+    [StringLength(300)] //tamanho maximo padrao de 300
     public string? ImagemUrl { get; set; }
     public ICollection<Produto>? Produtos { get; set; } //uma categoria pode ter muitos produtos. Eu acho que a classe q tem o ICollection é tida como a classe principal. 
     
--- APICatalogo/APICatalogo/Models/Produto.cs
+++ APICatalogo/APICatalogo/Models/Produto.cs
@@ -1,12 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace APICatalogo.Models;
 
 public class Produto
 {
     public int ProdutoId { get; set; } //chave primária
+    
+    [Required] //campo obrigatorio
+    [StringLength(80)] //tamanho max de 80
     public string? Nome { get; set; } //string é tipo por referencia, por isso precisa do ? q deve ser nullable
+    
+    [Required] //campo not null
+    [StringLength(300)] //tamanho max de 300
     public string? Descricao { get; set; }
+    
+    [Column(TypeName = "decimal(10,2)")] //definiu o tamanho de 10 digitos com 2 casas decimais. o Typename é decimal
     public decimal Preco { get; set; }
+    
+    [Required]
+    [StringLength(300)]
     public string? ImagemUrl { get; set; }
+    
     public float Estoque { get; set; }
     public DateTime DataCadastro { get; set; }
     #region ef, classes anemicas
