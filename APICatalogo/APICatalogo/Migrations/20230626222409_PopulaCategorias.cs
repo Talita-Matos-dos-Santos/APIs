6fde0e03a3b2659c91630dd1be220d1003ecf580
Talita Matos <talitagvmatos@gmail.com>
2023-06-26 19:45:45 -0300

    adicionando dados nas tabelas Categorias e Produtos através do método Up() de um Migration criado vazio

---
+++ APICatalogo/APICatalogo/Migrations/20230626222409_PopulaCategorias.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    public partial class PopulaCategorias : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Categorias(Nome, ImagemUrl) Values ('Bebidas', 'bebidas.jpg')"); //uso uma instancia de migrationbuilder e uso um metodo chamado Sql que vai executar uma instrução SQL no SGBD. Entre aspas eu tenho que por a instruçao sql. Primeiro eu coloco o nome das colunas que quero incluir e depois os valores
+            mb.Sql("Insert into Categorias(Nome, ImagemUrl) Values('Lanches', 'lanches.jpg')");
+            mb.Sql("Insert into Categorias(Nome, ImagemUrl) Values('Sobremesas', 'sobremesas.jpg')");
+
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Categorias");
+        }
+    }
+}
